67086dbc85ac68e424a724ff8fa4ee0a6ecb5ea0
manojchandrupm <manoj27092004@gmail.com>
2024-06-30 19:05:47 +0530

    update

--- assignment_1.py
+++ assignment_1.py
@@ -9,9 +9,9 @@ def random_value(inventory):
         inventory -= sale
         sales.append(sale)
     return sales
+
 today = datetime.date.today()    
 def create_csv():
-     
      with open("categories.csv", 'r') as csvfile,open(f"monthly_sales_report - {today}.csv", 'w', newline='') as outfile:
         reader = csv.reader(csvfile)
         writer = csv.writer(outfile)
@@ -19,13 +19,11 @@ def create_csv():
         writer.writerow([f"Period: {today} - {today}"])
         writer.writerow(["No","Productname","categories","Initialinventory","week #1","week #2","week #3","week #4"])
         for row in reader:
-            No = row[0]
-            Productname = row[1]
-            categories = row[2]
             Initialinventory = row[3] 
             inventory=int(Initialinventory)
             sales = random_value(inventory)
-            writer.writerow([No,Productname,categories,Initialinventory] + sales)
+            #No = row[0],Productname = row[1],Productname = row[1]
+            writer.writerow([row[0],row[1],row[2],row[3]] + sales)
 create_csv()
 line = open(f"monthly_sales_report - {today}.csv")
 print(line.read())
